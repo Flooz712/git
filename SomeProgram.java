2a4bb4375a9dc22292135bcdb28f7e1f2efb026b
Jan Kowalczyk <dsw52712@student.dsw.edu.pl>
2024-04-21 15:35:34 +0200

    Skeleton of the code to manage a satellite

--- SomeProgram.java
+++ SomeProgram.java
@@ -3,4 +3,7 @@ public class SomeProgram {
 		System.out.printIn("We are learning to use Git.");
 		System.out.printIn("We will be Git experts, soon.");
 	}
+	public void manageTheSatellite() {
+		//TODO implement
+	}
 }
